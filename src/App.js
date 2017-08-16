2c92e71f1257fa3d12a5f1e636b22ac9231b0727
Nessa <rc.vanessa87@gmail.com>
2017-08-16 20:33:06 +0100

    Initial commit of the functional app

---
+++ src/App.js
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor() {
+    super(); 
+    this.handleChanges = this.handleChanges.bind(this);
+    this.initialSetup = this.initialSetup.bind(this);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      player: 0,
+    };
+  }
+   
+  initialSetup() {
+     this.setState({
+             squares: Array(9).fill(null),
+             xIsNext: ({
+                checked: false,
+             }),
+            player: 0,
+      });
+     return this.setState;
+  }
+  
+  handleChanges(e){ 
+    this.setState({
+      xIsNext: e.target.value === 'X',
+      player: e.target.value,
+    });
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (this.state.player === 0) {
+        this.setState({
+             squares: Array(9).fill(null),
+        });
+     return this.setState;
+    }
+    if (calculateWinner(squares)) {
+      return;
+    }  else if (squares.indexOf(null) === -1 && !calculateWinner(squares)) {
+      this.status = 'Its a tie!';
+      this.initialSetup();
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+
+  }
+  
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const player = this.player;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="container">
+        <div className="settings-game row">
+          <div className="col-xs-12 col-md-12 col-lg-12 center">
+            <h3>Tic Tac Toe game</h3>
+            <p>Choose which one you want to play:</p>
+            <ul>
+            <li>
+            <label>
+                <input id="X" 
+                    type="radio"
+                    name="player"
+                    value = 'X'
+                    disabled = {this.state.player}
+                    checked={this.state.player === 'X'}
+                    onChange={this.handleChanges}
+                />
+                &nbsp; I am 'X'
+            </label>
+            </li>
+            <li>
+            <label>
+                <input id="O" 
+                        type="radio" 
+                        name="player"  
+                        value='O'
+                        disabled = {this.state.player}
+                        checked={this.state.player === 'O'}
+                        onChange={this.handleChanges}
+                    />
+                &nbsp; I am 'O'
+            </label>
+            </li>
+          </ul>
+        </div>
+        </div>
+        <div className="status col-xs-12 col-md-12 col-lg-12 center">{status}</div>
+        <div className="board-row col-xs-12 col-md-12 col-lg-12 center">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row col-xs-12 col-md-12 col-lg-12 center">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row col-xs-12 col-md-12 col-lg-12 center">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div className="col-xs-12 col-md-12 col-lg-12 center"> 
+          <button type='submit'  onClick={this.initialSetup} className="submit-button">
+            Clear game 
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+// changes for the settings player of the game 
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// ========================================
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+export default Game;
